b6025d7ecca162a39da2620ca9c65e26299a2f54
Kamrul Hasan <42377369+kaamrulhaasaan@users.noreply.github.com>
2018-09-19 23:19:14 +0600

    Add files via upload
    This program is to understand the Class

---
+++ Mobile.java
@@ -0,0 +1,21 @@
+/*
+ * this is a simple program to understand class and object
+ */
+public class Mobile {
+	String name;
+	String model;
+	int price;
+}
+
+/*
+ * here, Mobile is a class.The class "Mobile" contains basically
+ * two things.
+ * 1.Data (variables or state)
+ * 2.Subroutines(Methods)
+ * A class is a blueprint of an object
+ * that means class contains the attributes of object.
+ * Here, the class Mobile has some attributes like name, model, price
+ * actually these are the attributes of any of its object like Nokia
+ * mobile or Apple mobile or BlackBerry mobile etc.Lets try to 
+ * understand object.for this, see the MobileTest class
+ */
