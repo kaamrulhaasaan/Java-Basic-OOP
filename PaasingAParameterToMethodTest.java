bbfa8a74dc0841e941b72091a7f97be410e87e3d
Kamrul Hasan <42377369+kaamrulhaasaan@users.noreply.github.com>
2018-09-21 15:42:19 +0600

    Add files via upload

---
+++ PaasingAParameterToMethodTest.java
@@ -0,0 +1,24 @@
+/*
+ * Author: Md. Kamrul Hasan
+ * CSE 13th Batch, PSTU
+ */
+/*
+ * this is a simple java program to understand how to pass a
+ * parameter to a method
+ */
+
+public class PaasingAParameterToMethodTest {
+
+	public static void main(String[] args) {
+		//lets create an object first
+		PassingAParameterToMethod parameter1 = new PassingAParameterToMethod();
+		
+		//lets call the method
+		parameter1.Speak("Hi, my name is Kamrul Hasan");
+		/*
+		 * congratulations, you have successfully passed the value
+		 * in the Speak method
+		 */
+	}
+
+}
