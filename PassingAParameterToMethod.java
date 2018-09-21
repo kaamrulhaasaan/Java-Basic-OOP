326940cbe757ee567c1a7c8d72d542ec5f86357b
Kamrul Hasan <42377369+kaamrulhaasaan@users.noreply.github.com>
2018-09-21 15:41:47 +0600

    Add files via upload

---
+++ PassingAParameterToMethod.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Md. Kamrul Hasan
+ * CSE 13th Batch, PSTU
+ */
+/*
+ * this is a simple java program to understand how to pass a
+ * parameter to a method
+ */
+public class PassingAParameterToMethod {
+	public void Speak(String text){
+		System.out.println(text);
+	}
+	
+	/*
+	 * in the upper method, we have passed a parameter
+	 * named text to the method Speak
+	 * we need to write so text in the () when we call the method
+	 */
+}
