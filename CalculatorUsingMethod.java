7e381f9c7c13bda605d83c05000a8e5b7c25eb40
Kamrul Hasan <42377369+kaamrulhaasaan@users.noreply.github.com>
2018-09-21 16:00:45 +0600

    Add files via upload

---
+++ CalculatorUsingMethod.java
@@ -0,0 +1,30 @@
+/*
+ * Author: Md. Kamrul Hasan
+ * CSE 13th Batch, PSTU
+ */
+/*
+ * this is a simple java program to make a calculator by using
+ * method
+ */
+public class CalculatorUsingMethod {
+	
+	//this method is to add two numbers
+	public void Add(double number1,double number2){
+		System.out.println("Addition of these numbers is "+(number1+number2));
+	}
+	
+	//this method is to subtract two numbers
+	public void Subtract(double number1,double number2){
+		System.out.println("Subtraction of these numbers is "+(number1-number2));
+	}
+	
+	//this method is to multiply two numbers
+	public void Multiply(double number1,double number2){
+		System.out.println("Multiplication of these numbers is "+(number1*number2));
+	}
+	
+	//this method is to divide two numbers
+	public void Divide(double number1,double number2){
+		System.out.println("Division of these numbers is "+(number1/number2));
+	}
+}
