04b1e7433067d72577753f48801a90261cb63876
Kamrul Hasan <42377369+kaamrulhaasaan@users.noreply.github.com>
2018-09-21 06:53:54 +0600

    Add files via upload

---
+++ GetMethod.java
@@ -0,0 +1,21 @@
+/*
+ * Author: Md. Kamrul Hasan
+ * CSE 13th Batch, PSTU
+ */
+/*
+ * this is a simple java program to understand Get method
+ */
+public class GetMethod {
+	String name;
+	int age;
+	
+	//this method will return the name
+	String GetName(){
+		return name;
+	}
+	
+	//this method will return the name
+	int GetAge(){
+		return age;
+	}
+}
